a06081359fc2ec2073c83056efbcefb8c224a0e6
Haris Iqbal <harisiqbal7465@gmail.com>
2021-06-26 17:27:35 +0500

    Refactor

--- SharePal/app/src/main/java/com/hidevelopers/sharepal/presentation/ui/share/ShareViewModel.kt
+++ SharePal/app/src/main/java/com/hidevelopers/sharepal/presentation/ui/share/ShareViewModel.kt
@@ -1,71 +1,62 @@
-package com.hidevelopers.sharepal.presentation.ui.share//package com.hidevelopers.sharepal.presentation.ui.share
+package com.hidevelopers.sharepal.presentation.ui.share
 
-
-import android.app.Application
-import androidx.lifecycle.*
-import com.hidevelopers.sharepal.repository.StorageRepo
+import android.content.Context
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hhdeveloper.sharingapplication.datasource.data.AppData
+import com.hidevelopers.sharepal.repository.ExternalStorageRepository
 import com.hidevelopers.sharepal.repository.data.AudioData
 import com.hidevelopers.sharepal.repository.data.ImageData
 import com.hidevelopers.sharepal.repository.data.VideoData
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import timber.log.Timber
+import javax.inject.Inject
 
-class ShareViewModel(application: Application) : AndroidViewModel(application) {
 
-    private var repository: StorageRepo = StorageRepo(application)
-    private var dispatcher: CoroutineDispatcher = Dispatchers.Main
+@HiltViewModel
+class ShareViewModel @Inject constructor(
+    private val repository: ExternalStorageRepository,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Main
+):ViewModel() {
     private val _getAllVideoList = MutableLiveData<List<VideoData>>()
-    val getAllVideoList: LiveData<List<VideoData>> = getVideoList()
+    val getAllVideoList: LiveData<List<VideoData>> = _getAllVideoList
 
-    private val _name = MutableLiveData("")
-    val name: LiveData<String> = _name
-//
-//    private val _getAllAudioList = MutableLiveData<List<AudioData>>()
-//    val getALlAudioList: LiveData<List<AudioData>> = _getAllAudioList
-//
-//    private val _getALlImageList = MutableLiveData<List<ImageData>>()
-//    val getAllImageList: LiveData<List<ImageData>> = _getALlImageList
-    //private val videoLiveDataList:MutableLiveData<List<VideoData>> =MutableLiveData()
-    private val imageLiveDataList:MutableLiveData<List<ImageData>> = MutableLiveData()
-    private val audioLiveDataList:MutableLiveData<List<AudioData>> = MutableLiveData()
+    private val _getAllAudioList = MutableLiveData<List<AudioData>>()
+    val getALlAudioList: LiveData<List<AudioData>> = _getAllAudioList
 
-//    private val _getAllAppInstallList = MutableLiveData<List<AppData>>()
-//    val getALlAppInstallList: LiveData<List<AppData>> = _getAllAppInstallList
+    private val _getALlImageList = MutableLiveData<List<ImageData>>()
+    val getAllImageList: LiveData<List<ImageData>> = _getALlImageList
 
-    private fun getVideoList():MutableLiveData<List<VideoData>> {
-        getAllVideoList()
-        return _getAllVideoList
-    }
+    private val _getAllAppInstallList = MutableLiveData<List<AppData>>()
+    val getALlAppInstallList: LiveData<List<AppData>> = _getAllAppInstallList
 
     private fun getAllVideoList(){
         viewModelScope.launch(dispatcher) {
-            Timber.i("repository ${repository.getVideosList()}")
-            _getAllVideoList.postValue(
-                withContext(Dispatchers.IO){
-                    repository.getVideosList()
-                }
-            )
+            _getAllVideoList.postValue(repository.getVideosList())
         }
     }
-//
-//    private fun getImageList(){
-//        viewModelScope.launch(dispatcher) {
-//            _getALlImageList.postValue(repository.getImagesList())
-//        }
-//    }
-//
-//    private fun getAudioList(){
-//        viewModelScope.launch(dispatcher) {
-//            _getAllAudioList.postValue(repository.getAudiosList())
-//        }
-//    }
 
-//    private fun getAppInstallList(){
-//        viewModelScope.launch(dispatcher) {
-//            _getAllAppInstallList.postValue(repository.getInstallApp())
-//        }
-//    }
+    private fun getImageList(){
+        viewModelScope.launch(dispatcher) {
+            _getALlImageList.postValue(repository.getImagesList())
+        }
+    }
+
+    private fun getAudioList(){
+        viewModelScope.launch(dispatcher) {
+            _getAllAudioList.postValue(repository.getAudiosList())
+        }
+    }
+
+    private fun getAppInstallList(){
+        viewModelScope.launch(dispatcher) {
+            _getAllAppInstallList.postValue(repository.getInstallApp())
+        }
+    }
 }
